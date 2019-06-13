0b818b3bb85bb657030a3aa41180d3cfea700f24
Dtrung1999 <51434761+Dtrung1999@users.noreply.github.com>
2019-06-14 06:41:45 +0700

    Update xuly.php

--- xuly.php
+++ xuly.php
@@ -7,32 +7,32 @@
 <body>
 <?php
  
-    // Nếu không phải là sự kiện đăng ký thì không xử lý
+    
     if (!isset($_POST['txtUsername'])){
         die('');
     }
      
-    //Nhúng file kết nối với database
+    
     include('conn_acc.php');
           
-    //Khai báo utf-8 để hiển thị được tiếng việt
+    
     header('Content-Type: text/html; charset=UTF-8');
           
-    //Lấy dữ liệu từ file dangky.php
+    
     $username   = addslashes($_POST['txtUsername']);
     $password   = addslashes($_POST['txtPassword']);
           
-    //Kiểm tra người dùng đã nhập liệu đầy đủ chưa
+    
     if (!$username || !$password )
     {
         echo "Vui lòng nhập đầy đủ thông tin. <a href='javascript: history.go(-1)'>Trở lại</a>";
         exit;
     }
           
-        // Mã khóa mật khẩu
+        
         $password = md5($password);
           
-    //Lưu thông tin thành viên vào bảng
+    
     @$addmember = mysqli_query($conn,"
         INSERT INTO acc (
             Username,
@@ -44,7 +44,7 @@
         )
     ");
                           
-    //Thông báo quá trình lưu
+    
     if ($addmember)
         echo "Quá trình đăng ký thành công. <a href='index.php'>Về trang chủ</a>";
     else
