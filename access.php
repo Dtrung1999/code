e7ce2a8ea678f48d1895cdce065901c43143145d
Dtrung1999 <51434761+Dtrung1999@users.noreply.github.com>
2019-06-14 06:38:55 +0700

    Update access.php

--- access.php
+++ access.php
@@ -1,37 +1,34 @@
 <?php
-//Khai báo sử dụng session
 session_start();
  
-//Khai báo utf-8 để hiển thị được tiếng việt
 header('Content-Type: text/html; charset=UTF-8');
  
-//Xử lý đăng nhập
 if (isset($_POST['dangnhap'])) 
 {
-    //Kết nối tới database
+    
     include('conn_acc.php');
      
-    //Lấy dữ liệu nhập vào
+    
     $username = addslashes($_POST['txtUsername']);
     $password = addslashes($_POST['txtPassword']);
      
-    //Kiểm tra đã nhập đủ tên đăng nhập với mật khẩu chưa
+    
     if (!$username || !$password) {
         echo "Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu. <a href='javascript: history.go(-1)'>Trở lại</a>";
         exit;
     }
      
-    // mã hóa pasword
+    
     $password = md5($password);
      
-    //Kiểm tra tên đăng nhập có tồn tại không
+   
     $query = mysqli_query($conn,"SELECT * FROM acc WHERE Username='$username'");
     if (mysqli_num_rows($query) == 0) {
         echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
         exit;
     }
      
-    //Lưu tên đăng nhập
+   
     $_SESSION['username'] = $username;
     echo "Xin chào " . $username . ". Bạn đã đăng nhập thành công. <a href='index.php'>Về trang chủ</a>";
     die();
